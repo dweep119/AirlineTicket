66e27531df23917f704c1a9125677df1b4a35ef9
Dweep Patel <dweeppatel123@gmail.com>
2022-11-23 19:04:37 +0530

    create a table and add one record

---
+++ airline-project/src/api/airline-ticket/controllers/airline-ticket.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * airline-ticket controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::airline-ticket.airline-ticket');
---
+++ airline-project/src/api/airline-ticket/routes/airline-ticket.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * airline-ticket router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::airline-ticket.airline-ticket');
